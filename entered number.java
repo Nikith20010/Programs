b4b5b2a030d07d4ce16d49fec7adc33da168ad28
Nikith20010 <85982318+Nikith20010@users.noreply.github.com>
2021-12-10 12:29:20 +0530

    Create entered number.java

---
+++ entered number.java
@@ -0,0 +1,18 @@
+package nothing;
+
+import java.util.Scanner;
+
+public class Pgm2 {
+
+	public static void main(String[] args) {
+		
+		int  x;
+		Scanner sc=new  Scanner(System.in);
+		System.out.println("Enter the numbers");
+		x=sc.nextInt();
+		System.out.println("Entered Number is: "+x);
+		// TODO Auto-generated method stub
+
+	}
+
+}
