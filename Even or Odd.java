5784156c4399588532581db99f6227d758c4b862
Nikith20010 <85982318+Nikith20010@users.noreply.github.com>
2021-12-10 12:22:43 +0530

    Create Even or Odd.java

---
+++ Even or Odd.java
@@ -0,0 +1,20 @@
+package nothing;
+
+import java.util.Scanner;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		num=sc.nextInt();
+		if (num % 2 == 0)
+		System.out.println("number is even");
+		else
+		System.out.println("number is odd");
+		// TODO Auto-generated method stub
+
+	}
+
+}
